d2dc6eed5f82919bd7773a058c6dba76e56a3f93
Roy Margalit <roy4499@gmail.com>
2020-10-11 21:31:17 +0300

    second commit only styling left

---
+++ js/controller.js
@@ -0,0 +1,102 @@
+'use strict'
+console.log('Starting up');
+
+function init() {
+    renderPortfolio()
+}
+
+
+function renderPortfolio() {
+    var strHtml = ''
+    gProj.forEach(function (proj) {
+        strHtml += `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal" onclick="renderModal()" href="#portfolioModal${proj.modal}">
+          <div class="portfolio-hover">
+          <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+          </div>
+          </div>
+          <img class="img-fluid" src="img/renderportfolio/${proj.id}.jpg" alt="">
+          </a>
+          <div class= "portfolio-caption">
+          <h4>${proj.name}</h4>
+          <p class="text-muted">${proj.title}</p>
+          </div>
+          </div> `
+        return strHtml
+    })
+    var elPortfolio = document.querySelector('.container-portfolio .row')
+    return elPortfolio.innerHTML += strHtml
+}
+
+function renderModal() {
+    var strHtml = ''
+    gProj.forEach(function (proj) {
+        console.log('proj', proj)
+        strHtml += `<div class="portfolio-modal modal fade" id="portfolioModal${proj.modal}" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+              <div class="lr">
+                <div class="rl"></div>
+              </div>
+            </div>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8 mx-auto">
+                  <div class="modal-body">
+                    <h2>${proj.name}</h2>
+                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                    <img class="img-fluid d-block mx-auto" src="img/renderportfolio/${proj.id}.jpg" alt="">
+                    <p>${proj.desc}</p>
+                    <ul class="list-inline">
+                      <li>Date: January 2017</li>
+                      <li>Client: Threads</li>
+                      <li>Category: Illustration</li>
+                    </ul>
+                    <button class="btn btn-proj" onclick="onShowProj(${proj.modal})" type="button">
+                      <i class="fa fa-times"></i>
+                      Show Me!</button>
+                    <button class="btn btn-primary" data-dismiss="modal" type="button">
+                      <i class="fa fa-times"></i>
+                      Close Project</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    `
+    return strHtml
+    })
+    var elContainer = document.querySelector('.portfolio-container')
+    elContainer.innerHTML += strHtml
+}
+
+
+
+
+
+function onShowProj(projNum) {
+    gProj.forEach(function (proj) {
+        if (projNum === proj.modal) {
+            window.open(`${proj.url}`)
+
+            console.log(projNum)
+        }
+
+    })
+    console.log(gProj)
+}
+
+function onContact() {
+    var email = document.getElementById('exampleInputEmail1').value
+    console.log(email)
+    var subject = document.getElementById('exampleInputSubject').value
+    console.log(subject)
+    window.open(`https://mail.google.com/mail/u/0/?view=cm&fs=1&to=me@example.com&su=${subject}&body=BODY&bcc=someone.${email}@example.com&tf=1`)
+
+}
+
+
---
+++ js/services.js
@@ -0,0 +1,44 @@
+var gProj = [
+    {
+        id: 'mineSweeper',
+        name: 'Mine Sweeper',
+        title: 'Minesweeper is a single-player puzzle video game.',
+        desc: 'Minesweeper is a single-player puzzle video game. The objective of the game is to clear a rectangular board containing hidden "mines" or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and has been written for many computing platforms in use today. It has many variations and offshoots.',
+        url: 'projs/mines\ css/index.html',
+        publishedAt: 'nan',
+        labels: ['matrixes', 'keyboard events'],
+        modal: 1,
+
+    },
+    {
+        id: 'touchNums',
+        name: 'Touch the nums',
+        title: 'This is a game you need to count until 16 to win',
+        desc: 'he 1960s, and has been written for many computing platforms in use today. It has many variations and offshoots jofh;aioh fdkhs;goj dkghaskfna/kln ewdghasgh hsg;ij lasdgh jes hdfkj a/lja/rg tojrgo jrgp rogj op.',
+        url: 'projs/touch\ the\ num\ css/index.html',
+        publishedAt: 'nan',
+        labels: ['matrixes', 'keyboard events'],
+        modal: 2
+
+    },
+    {
+        id: 'guessMe',
+        name: 'Guess ME',
+        title: 'Guess who and add questions!',
+        desc: `'Akinator's all-consuming passion is trying to guess characters by asking questions.
+        To play with him, think of a character, real or fictional, keep it well in mind and then click on the
+        menu "play > characters".
+        Akinator will then proceed to ask you a series of questions that you'll have to answer as truthfully
+        as possible. After this series of questions, he will tell you what you'`,
+        url: 'projs/GuessMe-Ex-startHere-1.1/index.html',
+        publishedAt: 'nan',
+        labels: ['matrixes', 'keyboard events'],
+        modal: 3,
+    },
+]
+
+function getProjectById(id) {
+    return gProjects.find(function (project) {
+        return project.id === id;
+    })
+}
